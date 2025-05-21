6ea7640cdf6c2b9fe27f8e2259b612ba4d8b5c4d
LucaoCorrea <lucascorrea1505@gmail.com>
2025-05-21 15:25:43 -0300

    Patients: Controller, endpoints, service and methods

---
+++ sentinel/src/main/java/com/springboot/sentinel/controller/PatientController.java
@@ -0,0 +1,47 @@
+package com.springboot.sentinel.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.sentinel.model.Patient;
+import com.springboot.sentinel.service.PatientService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+    @Autowired
+    private PatientService patientService;
+
+    @PostMapping
+    public Patient savePatient(@RequestBody Patient patient) {
+        return patientService.savePatient(patient);
+    }
+
+    @GetMapping
+    public List<Patient> listPatient() {
+        return patientService.listPatients();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Patient> findPatientById(@PathVariable Long id) {
+        return patientService.findPatientById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delPatient(@PathVariable Long id) {
+        patientService.delPatient(id);
+    }
+
+}
--- sentinel/src/main/java/com/springboot/sentinel/service/PatientService.java
+++ sentinel/src/main/java/com/springboot/sentinel/service/PatientService.java
@@ -1,8 +1,9 @@
 package com.springboot.sentinel.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.springboot.sentinel.model.Patient;
 import com.springboot.sentinel.repository.PatientRepository;
@@ -16,7 +17,19 @@ public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
 
-    public Patient savePatient(PatientRepository patientRepository) {
-        return patientRepository.save(patientRepository);
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public List<Patient> listPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Optional<Patient> findPatientById(Long id) {
+        return patientRepository.findById(id);
+    }
+
+    public void delPatient(Long id) {
+        patientRepository.deleteById(id);
     }
 }
